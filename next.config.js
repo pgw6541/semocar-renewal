08dc6a6830967df18f60a8d6002e821ece5ce3ec
pgw6541 <96gunw@gmail.com>
2024-01-04 16:28:17 +0900

    update next config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {}
 const { webpack } = require('next/dist/compiled/webpack/webpack')
 
 module.exports = {
-  // webpack: (config, {dev, isServer}) => {
-  //   if(dev){   
-  //       // 개발 환경 설정
-  //   } else {
-  //     // 프로덕션 환경 설정
-  //   }
-  //   return config
-  // }
+  webpack: (config, {dev, isServer}) => {
+    if(dev){   
+        // 개발 환경 설정
+    } else {
+      // 프로덕션 환경 설정
+    }
+    return config
+  }
 }

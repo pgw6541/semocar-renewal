dcc7a92fdf0845347258ee66d264a76eedf7e986
pgw6541 <96gunw@gmail.com>
2024-01-08 16:07:08 +0900

    하단 nav 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.scss'
 
-import Footer from './components/footer/footer'
+import Footer from '../components/footer/footer'
+import Nav from '../components/nav/nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Footer />
+        <Nav />
       </body>
     </html>
   )

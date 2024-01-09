e6c9638f1671ef7055688fd3c404f557a26d65b4
pgw6541 <96gunw@gmail.com>
2024-01-09 17:53:57 +0900

    옵션선택 Modal 생성

---
+++ app/search/modal.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from "react";
+import search from "./search.module.scss"
+
+type ModalProps = {
+  show: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({show, onClose, children}) => {
+  // false값이 들어오면 null값을 리턴
+  if(!show){
+    return null
+  }
+
+  return(
+    <div className={search.modal}>
+      <div className={search.modal_content}>
+        <span className={search.close} onClick={onClose}>&times;</span>
+        {children}
+      </div>
+    </div>
+  )
+}
